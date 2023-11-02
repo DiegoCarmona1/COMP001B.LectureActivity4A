857447c427c697800ed57e9fb3d768bb66a43bd5
Diego Carmona <diegocarmona@my.whccd.edu>
2023-11-02 00:37:26 -0700

    added input variable and if else statements

---
+++ script.js
@@ -0,0 +1,13 @@
+let myGrade = 72
+if (myGrade >= 90){
+    console.log(A);
+} else if (myGrade == 80-89) {
+    console.log(B);
+} else if (myGrade == 70-79) {
+    console.log(C);
+} else if (myGrade == 60-69) {
+    console.log(D);
+} else if (myGrade <= 59) {
+    console.log(F);
+}
+
