46a918d4a75ca9572c0dbc6e77799be0746b44ac
Diego Carmona <diegocarmona@my.whccd.edu>
2023-11-02 00:42:30 -0700

    added quotation marks to consol log statements

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 let myGrade = 72
 if (myGrade >= 90){
-    console.log(A);
+    console.log("A");
 } else if (myGrade == 80-89) {
-    console.log(B);
+    console.log("B");
 } else if (myGrade == 70-79) {
-    console.log(C);
+    console.log("C");
 } else if (myGrade == 60-69) {
-    console.log(D);
+    console.log("D");
 } else if (myGrade <= 59) {
-    console.log(F);
+    console.log("F");
 }
 
