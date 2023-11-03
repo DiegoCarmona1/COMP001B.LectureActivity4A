e305e9ffd485755e4910efbf477ed13026e21f37
Diego Carmona <diegocarmona@my.whccd.edu>
2023-11-03 00:49:44 -0700

    edited for loop to make odds !

--- script.js
+++ script.js
@@ -71,5 +71,7 @@ switch (myGrade) {
         break;
 }
 for (let number = 0; number <= 100; number = number + 1) {
+    if (number % 2 == 0){
+    console.log("!")
     console.log(number)
-}
+    }}
